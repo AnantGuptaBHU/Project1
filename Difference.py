7f80d2cb94820dd9c23bfcdfb7f2897709b01746
Anant Gupta <anant.varanasi@gmail.com>
2022-07-13 08:42:24 +0530

    changes in mul and diff

--- Difference.py
+++ Difference.py
@@ -1,3 +1,4 @@
 class operation():
     def subtraction(self,a,b):
         print("a-b")
+        print("thus is a product of two numbers")
--- Multiplication.py
+++ Multiplication.py
@@ -1,3 +1,5 @@
 class operation1():
     def multiply(self,a,b):
         print(a*b)
+        print("this is a multiplication")
+        
